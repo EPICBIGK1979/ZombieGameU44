32177039327e4e71763eceddf90785ee94be688c
EPICBIGK1979 <pil23006623@heartofyorkshire.ac.uk>
2024-03-07 09:44:02 +0000

    Scoring System
    Works and once score is achieved new level is loaded

--- U44ZombieGame/Assets/Assets/Scripts/Raycast_Gun.cs
+++ U44ZombieGame/Assets/Assets/Scripts/Raycast_Gun.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
+ 
 public class Raycast_Gun : MonoBehaviour
 {
     public float gunDamage, gunRange, fireRate, reloadTime;
     public int magSize, currentAmmo, reserveAmmo, impactForce;
-
+ 
     public GameObject impactEffect;
     public ParticleSystem muzzleFlash;
-    
+ 
     public TMP_Text ammoText;
-
+    public TMP_Text scoreText;
+    public int score = 50;
+   
     private float nextFire;
-
+ 
+   
+ 
+ 
+ 
+ 
+ 
     // Start is called before the first frame update
     void Start()
     {
+        scoreText.text = "Score: " + score;
         currentAmmo = magSize;
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
@@ -28,22 +39,22 @@ public class Raycast_Gun : MonoBehaviour
             Shoot();
             currentAmmo--;
         }
-
+ 
         if(currentAmmo <= 0 && reserveAmmo > 0){
             Reload();
         }
-
+ 
         if(Input.GetKeyDown(KeyCode.R)){
             reserveAmmo = reserveAmmo + currentAmmo;
             currentAmmo = 0;
         }
         ammoText.SetText(currentAmmo.ToString() + " / " + reserveAmmo.ToString());
     }
-
-
+ 
+ 
     void Shoot(){
         muzzleFlash.Play();
-
+ 
         RaycastHit hit;
         nextFire = Time.time + fireRate;
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, gunRange)){
@@ -51,17 +62,18 @@ public class Raycast_Gun : MonoBehaviour
             Enemy_Target target = hit.transform.GetComponent<Enemy_Target>();
             if(target != null){
                 target.TakeDamage(gunDamage);
+                 AddScore();
             }
             if(hit.rigidbody != null){
                 hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
-
+ 
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO, 2f);
         }
-
+ 
     }
-
+ 
     void Reload(){
         nextFire = Time.time + reloadTime;
         if(reserveAmmo > magSize){
@@ -73,4 +85,13 @@ public class Raycast_Gun : MonoBehaviour
             reserveAmmo = 0;
         }
     }
+ 
+      void AddScore(){
+        score++;
+        scoreText.text = "Score: " + score;
+        if(score >=20){
+            SceneManager.LoadScene(+1);
+        }
+    }
 }
+ 
